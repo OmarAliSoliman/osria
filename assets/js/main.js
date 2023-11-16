71be786afdc7555258434f59b2f0314dbfb0fc1a
OmarAliSoliman <omarasmar9955@gmail.com>
2023-11-16 10:55:29 +0200

    pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,24 +34,30 @@ $(document).ready(function () {
       // autoplaySpeed: 1000,
       responsive: [
         {
-          breakpoint: 1024,
+          breakpoint: 1300,
           settings: {
             slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 1100,
+          settings: {
+            slidesToShow: 2.7,
             slidesToScroll: 1,
-            infinite: true,
+            infinite: false,
             dots: false,
           },
         },
         {
           breakpoint: 991,
           settings: {
-            slidesToShow: 3,
+            slidesToShow: 2.1,
           },
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 1,
+            slidesToShow:1,
           },
         },
       ],
@@ -77,6 +83,24 @@ $(document).ready(function () {
       infinite: false,
       responsive: [
         {
+          breakpoint: 1375,
+          settings: {
+            slidesToShow: 3.7,
+            slidesToScroll: 1,
+            infinite: false,
+            dots: false,
+          },
+        },
+        {
+          breakpoint: 1110,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false,
+          },
+        },
+        {
           breakpoint: 1024,
           settings: {
             slidesToShow: 3,
@@ -88,13 +112,13 @@ $(document).ready(function () {
         {
           breakpoint: 991,
           settings: {
-            slidesToShow: 2.5,
+            slidesToShow: 1.7,
           },
         },
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 1.4,
+            slidesToShow: 1,
           },
         },
       ],
